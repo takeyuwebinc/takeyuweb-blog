719a2b6bf7581c1f9d19b8bd3f8e0489172ac52f
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2019-10-27 17:25:35 +0900

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_24_140221) do
+ActiveRecord::Schema.define(version: 2019_10_27_081307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2019_07_24_140221) do
     t.bigint "byte_size", null: false
     t.string "checksum", null: false
     t.datetime "created_at", null: false
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
